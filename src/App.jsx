0c5921887ab711ccd13d1eca7b25789f8697483a
ing-mgiordano <ing.mgiordano@gmail.com>
2022-05-26 16:57:13 +0200

    upload

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
+
+
+
+function App() {
+  const [presupuesto, setPresupuesto] = useState(0)
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+
+  const [ventanaModal, setVentanaModal] = useState(false)
+  const handleNuevoGasto = () => {
+    setVentanaModal(true)
+  }
+
+  return (
+    <div>
+
+      <Header
+        presupuesto = { presupuesto }
+        setPresupuesto = { setPresupuesto }
+        isValidPresupuesto = { isValidPresupuesto }
+        setIsValidPresupuesto = { setIsValidPresupuesto }
+      />
+
+      {isValidPresupuesto && (   /* si el presupuesto es valido se imprime el icono de nuevo gasto */
+        <div  className='nuevo-gasto'>
+          <img 
+            src={IconoNuevoGasto} 
+            alt='icono nvo gasto' 
+            onClick={handleNuevoGasto}
+          />
+        </div>
+      )}
+
+      {ventanaModal && <p>SE ABRE VENTADA MODAL</p> }
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import Mensaje from './Mensaje'
+
+const NuevoPresupuesto = ({ 
+    presupuesto, 
+    setPresupuesto,
+    setIsValidPresupuesto
+}) => {
+
+    const [mensaje, setMensaje] = useState('')
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault()
+        
+        //presupuesto no valido
+        if(!presupuesto || presupuesto<= 0) {
+            setMensaje('No es un presupuesto valido')
+            return
+        }
+        setMensaje('')
+        setIsValidPresupuesto(true)
+
+        
+    }
+
+  return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+        <form onSubmit={handlePresupuesto} className='formulario'>  {/* onSubmit={handlePresupuesto} --> al presionar submit se ejecuta la funcion */}
+            <div className='campo'>
+                <label>Definir Presupuesto</label>
+
+                <input
+                    type='number' 
+                    className='nuevo-presupuesto'
+                    placeholder='Añade tu Presupuesto'
+                    value={presupuesto}
+                    onChange={e => setPresupuesto(Number(e.target.value))} // lee lo que el usuario va escribiendo
+                />
+            </div>
+
+            <input 
+                type='submit' 
+                value='Añadir' 
+            />
+            {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje>} {/* && (cuando mensaje sea verdadero ejecuta el codigo de la derecha).. es como un ternario pero con una sola condicion */}
+        </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
+
+
