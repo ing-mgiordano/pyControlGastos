5c66bd4a38f58d7866f3744d935af2c2398aa695
ing-mgiordano <ing.mgiordano@gmail.com>
2022-05-26 18:27:44 +0200

    se agrego formulario y useState en ventana modal

--- src/components/VentadaModal.jsx
+++ src/components/VentadaModal.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react'
 import CerrarBtn from '../img/cerrar.svg'
 
 const VentadaModal = ({ setVentanaModal, setAnimarModal, animarModal }) => {
+
+    const [nombre, setNombre] = useState('')
+    const [cantidad, setCantidad] = useState('')
+    const [categoria, setCategoria] = useState('')
 
     const ocultarModal = () => {
         setAnimarModal(false)  
@@ -12,17 +17,61 @@ const VentadaModal = ({ setVentanaModal, setAnimarModal, animarModal }) => {
 
   return (
     <div className='modal'>
-      <div className='cerrar-modal'>
-        <img 
-            src={CerrarBtn} 
-            alt="cerrar modal"
-            onClick={ocultarModal}
-        />
-      </div>
+        <div className='cerrar-modal'>
+            <img 
+                src={CerrarBtn} 
+                alt="cerrar modal"
+                onClick={ocultarModal}
+            />
+        </div>
 
-      <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
-        <legend>Nuevo Gasto</legend>
-      </form>
+        <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
+            <legend>Nuevo Gasto</legend>
+
+            <div className='campo'>
+                <label htmlFor='nombre'>Nombre Gasto</label>
+                <input
+                    id='nombre'
+                    type='text' 
+                    placeholder='Añade nombre del Gasto'
+                    value={nombre}
+                    onChange={e => setNombre(e.target.value)}
+                />
+            </div>
+
+            <div className='campo'>
+                <label htmlFor='cantidad'>Cantidad</label>
+                <input
+                    id='cantidad'
+                    type='number' 
+                    placeholder='Añade la cantidad del Gasto(€)'
+                    value={cantidad}
+                    onChange={e => setCantidad(Number(e.target.value))}
+                />
+            </div>
+
+            <div className='campo'>
+                <label htmlFor='categoria'>Categoria</label>
+                <select 
+                    id='categoria'
+                    value={categoria}
+                    onChange={e => setCategoria(e.target.value)}
+                >
+                    <option value=''>-- Seleccione --</option>
+                    <option value='comida'>Comida</option>
+                    <option value='casa'>Casa</option>
+                    <option value='gastos'>Gastos Varios</option>
+                    <option value='ocio'>Ocio</option>
+                    <option value='salud'>Salud</option>
+                    <option value='suscripciones'>Suscripciones</option>
+                </select>
+            </div>
+
+            <input
+                type='submit'
+                value='Añadir Gasto'
+            />
+        </form>
     </div>
   )
 }
